3d4a01336ea1290fc95214aaabdda926b2dfe19a
sidthakur08 <41535376+sidthakur08@users.noreply.github.com>
2020-10-31 23:36:52 +0530

    exploring

--- xGModel.R
+++ xGModel.R
@@ -1,5 +1,21 @@
 #install.packages("rjson")
 
 library(rjson)
+library(data.table)
+library(tidyverse)
+library(plyr)
 
 data <- fromJSON(file="data/events_England.json")
+print(data[[1]]$eventName)
+
+data_frame <- data.frame(do.call(rbind,data),stringAsFactors = FALSE)
+shot_data <- data_frame %>% filter(eventName=='Shot')
+r <- nrow(shot_data)
+c <- ncol(shot_data)
+
+shot_data[1,]
+
+# to get some idea about the dataframe
+glimpse(shot_data)
+# to get count of elements in list
+table(unlist(shot_data$playerId))
